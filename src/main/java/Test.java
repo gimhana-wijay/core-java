9ca46d86851b044c59dc160e372052030c59a4ca
gimhana <gsanadakelum@accelaero.com>
2022-07-04 11:13:15 +0530

    core-001 first commit

---
+++ src/main/java/Test.java
@@ -0,0 +1,16 @@
+package main.java;
+
+import java.util.ArrayList;
+
+import com.accelaero.aeropay.core.systemwide.Empty;
+
+public class Test {
+	public static void main(String args[]) {
+		ArrayList<String> aa = new ArrayList<>();
+		String a = null;
+		String b = "b";
+		if (Empty.isNotEmpty(a)) {
+			System.out.println(a);
+		}
+	}
+}
---
+++ src/main/java/dto/RequestDTO.java
@@ -0,0 +1,15 @@
+package main.java.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class RequestDTO {
+
+    private String id;
+    private String name;
+    private String disc;
+}
